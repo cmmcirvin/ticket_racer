d49b1271b89c97cc5a87e4e3e59ee48b451cf788
DaPickle <dillon.a.rose@gmail.com>
2024-09-14 12:50:25 -0400

    add home page

--- frontend/dorm_room_visualizer_frontend/src/pages/Home.tsx
+++ frontend/dorm_room_visualizer_frontend/src/pages/Home.tsx
@@ -1,12 +1,21 @@
 import { Outlet, redirect } from "react-router-dom";
 import { useEffect } from "react";
+import { Boxes } from "@/components/ui/background-boxes";
+import { cn } from "@/utils/cn";
 
 const Home = () => {
   
   return (
-    <div className="min-h-[53rem] w-full dark:bg-slate-950 bg-white  dark:bg-grid-slate-500/[0.2] bg-grid-black/[0.2]">
-      HIIII
-      <Outlet />
+    <div className="relative w-full h-[100vh] min-h-fit overflow-hidden bg-slate-900 flex flex-col items-center justify-center">
+      <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
+
+      <Boxes />
+      <h1 className={cn("md:text-4xl text-xl text-white relative z-20")}>
+        Tailwind is Awesome
+      </h1>
+      <p className="text-center mt-2 text-neutral-300 relative z-20">
+        Framer motion is the best animation library ngl
+      </p>
     </div>
 
   );
