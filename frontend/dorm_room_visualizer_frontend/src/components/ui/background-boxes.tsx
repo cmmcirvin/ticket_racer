3a2b48473d0d69c942095e1d2575a3e9125e3d96
DaPickle <dillon.a.rose@gmail.com>
2024-09-14 12:57:25 -0400

    white background

--- frontend/dorm_room_visualizer_frontend/src/components/ui/background-boxes.tsx
+++ frontend/dorm_room_visualizer_frontend/src/components/ui/background-boxes.tsx
@@ -7,15 +7,9 @@ export const BoxesCore = ({ className, ...rest }: { className?: string }) => {
   const rows = new Array(150).fill(1);
   const cols = new Array(100).fill(1);
   let colors = [
-    "--sky-300",
-    "--pink-300",
-    "--green-300",
-    "--yellow-300",
-    "--red-300",
-    "--purple-300",
-    "--blue-300",
-    "--indigo-300",
-    "--violet-300",
+    "--orange-500",
+    "--orange-400",
+    "--orange-600",
   ];
   const getRandomColor = () => {
     return colors[Math.floor(Math.random() * colors.length)];
@@ -35,7 +29,7 @@ export const BoxesCore = ({ className, ...rest }: { className?: string }) => {
       {rows.map((_, i) => (
         <motion.div
           key={`row` + i}
-          className="w-16 h-8  border-l  border-slate-700 relative"
+          className="w-16 h-8  border-l  border-gray-100 relative"
         >
           {cols.map((_, j) => (
             <motion.div
@@ -47,7 +41,7 @@ export const BoxesCore = ({ className, ...rest }: { className?: string }) => {
                 transition: { duration: 2 },
               }}
               key={`col` + j}
-              className="w-16 h-8  border-r border-t border-slate-700 relative"
+              className="w-16 h-8  border-r border-t border-gray-100 relative"
             >
               {j % 2 === 0 && i % 2 === 0 ? (
                 <svg
@@ -56,7 +50,7 @@ export const BoxesCore = ({ className, ...rest }: { className?: string }) => {
                   viewBox="0 0 24 24"
                   strokeWidth="1.5"
                   stroke="currentColor"
-                  className="absolute h-6 w-10 -top-[14px] -left-[22px] text-slate-700 stroke-[1px] pointer-events-none"
+                  className="absolute h-6 w-10 -top-[14px] -left-[22px] text-gray-100 stroke-[1px] pointer-events-none"
                 >
                   <path
                     strokeLinecap="round"
--- frontend/dorm_room_visualizer_frontend/src/pages/Home.tsx
+++ frontend/dorm_room_visualizer_frontend/src/pages/Home.tsx
@@ -6,8 +6,8 @@ import { cn } from "@/utils/cn";
 const Home = () => {
   
   return (
-    <div className="relative w-full h-[100vh] min-h-fit overflow-hidden bg-slate-900 flex flex-col items-center justify-center">
-      <div className="absolute inset-0 w-full h-full bg-slate-900 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
+    <div className="relative w-full h-[100vh] min-h-fit overflow-hidden bg-gray-300 flex flex-col items-center justify-center">
+      <div className="absolute inset-0 w-full h-full bg-gray-300 z-20 [mask-image:radial-gradient(transparent,white)] pointer-events-none" />
 
       <Boxes />
       <h1 className={cn("md:text-4xl text-xl text-white relative z-20")}>
